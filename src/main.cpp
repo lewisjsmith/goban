18966e04a3bd6873398cb081aa069b42f202e3ec
Lewis <sendto.lewissmith@gmail.com>
2026-06-08 11:34:22 +0100

    chore(main): merging commented main func to engine branch to continue work

--- src/main.cpp
+++ src/main.cpp
@@ -7,53 +7,53 @@
 
 int main(int argc, char* argv[]) {
 
-
     std::string cmd;
     Board* board = new Board();
 
-    while(std::getline(std::cin, cmd)) {
+    // while(std::getline(std::cin, cmd)) {
 
-        std::istringstream iss(cmd);
+    //     std::istringstream iss(cmd);
 
-        std::string action;
-        iss >> action;
+    //     std::string action;
+    //     iss >> action;
 
-        if(action == "play"){
-            std::string colour;
-            std::string move;
+    //     if(action == "play"){
+    //         std::string colour;
+    //         std::string move;
 
-            iss >> colour >> move;
+    //         iss >> colour >> move;
 
-            /*
-                Possible outcomes:
-                    Empty OK -> bool TRUE
-                    Empty but no liberties -> remove from board -> separation of set method and remove method.
-                    Full space -> bool FALSE
-            */
+    //         /*
+    //             Possible outcomes:
+    //                 Empty OK -> bool TRUE
+    //                 Empty but no liberties -> remove from board -> separation of set method and remove method.
+    //                 Full space -> bool FALSE
+    //         */
 
-            if(board->set(std::stoi(move), std::stoi(colour))) {
-                if(isAlive(*board, std::stoi(move))){
-                    // append updates to this message such as a delete group nearby 
-                    std::cout << "ok" << std::endl;
-                } else {
-                    // constant expressions for error strings 
-                    // this one is for dead stones / suicide stones
-                    board->set(std::stoi(move), 0);
-                    std::cout << "invalid dead" << std::endl;
-                }
-            } else {
-                std::cout << "invalid filled" << std::endl;
-            }
-        }
+    //         if(board->set(std::stoi(move), std::stoi(colour))) {
+    //             // How to check consecutive circles of pieces 
+    //             if(isAlive(*board, std::stoi(move))){
+    //                 // append updates to this message such as a delete group nearby 
+    //                 std::cout << "ok" << std::endl;
+    //             } else {
+    //                 // constant expressions for error strings 
+    //                 // this one is for dead stones / suicide stones
+    //                 board->set(std::stoi(move), 0);
+    //                 std::cout << "invalid dead" << std::endl;
+    //             }
+    //         } else {
+    //             std::cout << "invalid filled" << std::endl;
+    //         }
+    //     }
 
-        if(action == "new") {
-            board = new Board();
-        }
+    //     if(action == "new") {
+    //         board = new Board();
+    //     }
 
-        if(action == "quit") {
-            break;
-        }
-    }
+    //     if(action == "quit") {
+    //         break;
+    //     }
+    // }
 
     return 0;
 }
