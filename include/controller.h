d6d3cdde25d787d1dec44b7d8c0755b5c85f5a14
Lewis <sendto.lewissmith@gmail.com>
2026-06-08 11:29:11 +0100

    feat(controller): added dead stone detection for single and groups, invalid move cancel, with feedback string. fixed neighbour detection.

--- include/controller.h
+++ include/controller.h
@@ -5,10 +5,16 @@
 #include <vector>
 #include <unordered_map>
 #include <queue> 
+#include <sstream>
 
 #include <string>
+#include <algorithm>
 
-std::vector<unsigned int> getNeighbourPos(Board& board, unsigned int pos) {
+// debug
+#include <iostream>
+
+// getPositionsAroundStone
+std::vector<unsigned int> getNeighbours(Board& board, unsigned int pos) {
     
     std::vector<unsigned int> neighbours;
 
@@ -23,26 +29,16 @@ std::vector<unsigned int> getNeighbourPos(Board& board, unsigned int pos) {
         if(pos%width != 0) neighbours.push_back(pos - 1);
     } 
     if(pos < max){
-        if(pos%(width - 1) != 0 || pos == 0) neighbours.push_back(pos + 1);
+        if((pos-(width - 1))%width != 0 || pos == 0) neighbours.push_back(pos + 1);
     }
+
     if(pos >= width) neighbours.push_back(pos - width);
     if(pos <= max - width) neighbours.push_back(pos + width);
 
     return neighbours;
 }
 
-// get liberties func ? 
-/*
-    Basically,
-    Save the colour of the last move
-    Check if that last move has opposite colour liberty stones
-    Check if any of those opposite colour stones now have no liberties
-        If not, kill groups
-        Otherwise, check if the placed stone has no liberties
-            If not, kill placed stone
-        Else do nothing 
-*/
-
+// getGroupGivenSingleStone
 std::vector<unsigned int> getGroup(Board& board, unsigned int pos, char colour) {
 
     std::vector<unsigned int> group = {};
@@ -58,7 +54,7 @@ std::vector<unsigned int> getGroup(Board& board, unsigned int pos, char colour) 
         queue.pop();
         group.push_back(curr);
 
-        std::vector<unsigned int> neighbours = getNeighbourPos(board, curr);
+        std::vector<unsigned int> neighbours = getNeighbours(board, curr);
         
         for(auto&n : neighbours){
             if(seen.count(n) != 0) continue;
@@ -72,12 +68,20 @@ std::vector<unsigned int> getGroup(Board& board, unsigned int pos, char colour) 
     return group;
 }   
 
-std::vector<unsigned int> getLiberties(Board& board, std::vector<unsigned int> group, char colour) {
+// getGroupNeighboursGivenGroup
+std::vector<unsigned int> getGroupNeighbours(Board& board, std::vector<unsigned int> group, char colour) {
     std::vector<unsigned int> liberties = {};
     std::unordered_map<unsigned int, bool> seen = {};
 
     for(auto& s : group){
-        std::vector<unsigned int> neighbours = getNeighbourPos(board, s);
+        std::vector<unsigned int> neighbours = getNeighbours(board, s);
+
+        std::cout << "neighbours: ";
+        for(auto& n : neighbours) {
+            std::cout << int(n) << " ";
+        }
+        std::cout << "end of neighbours" << std::endl;
+
         for(auto& n : neighbours){
             if(seen.count(n) != 0) continue;
             if(board.get(n) != colour) liberties.push_back(n);
@@ -88,38 +92,102 @@ std::vector<unsigned int> getLiberties(Board& board, std::vector<unsigned int> g
     return liberties;
 }
 
-// std::vector<unsigned int> isAlive(Board& board, unsigned int pos) {
+// getLibertiesFromNeighbourList
+std::vector<unsigned int> getLiberties(Board& board, std::vector<unsigned int> groupNeighbours) {
+    
+    unsigned int emptyStoneCount;
 
-//     char group_colour = board.get(pos);
+    std::vector<unsigned int> liberties = {};
+    
+    for(auto& n : groupNeighbours) {
+        if(board.get(n) == 0){
+            emptyStoneCount++;
+            liberties.push_back(n);
+        }\
+    }
 
-//     std::unordered_map<unsigned int, bool> seen = {{pos, true}};
-//     std::vector<unsigned int> seen_group = {pos};
-//     std::queue<unsigned int> group_to_check;
-//     group_to_check.push(pos);
+    if(emptyStoneCount == 0) return {};
+    return liberties;
 
-//     while(group_to_check.size() > 0) {
+}
 
-//         unsigned int curr = group_to_check.front();
-//         seen_group.push_back(curr);
-//         group_to_check.pop();
+std::vector<unsigned int> getOppositeColourNeighbours(Board& board, std::vector<unsigned int> groupNeighbours) {
 
-//         std::vector<unsigned int> curr_neighbours = getNeighbourPos(board, curr);
-//         for(auto& n : curr_neighbours){
-//             if(seen.count(n) > 0) continue;
-//             char curr_neighbour = board.get(n);
-//             if(curr_neighbour == 0) return {};
-//             if(curr_neighbour == group_colour) group_to_check.push(n);
-//             seen[n] = true;
-//         }
+    std::vector<unsigned int> oppositeColourNeighbours = {};
+    
+    for(auto& n : groupNeighbours) {
+        if(board.get(n) != 0){
+            oppositeColourNeighbours.push_back(n);
+        }
+    }
 
-//     }
+    return oppositeColourNeighbours;
+}
 
-//     return false;
+// Consider swapping colours to 0 and 1 for easy falsy conversion
+std::string removeDeadStones(Board& board, unsigned int latestPos, char latestColour) {
 
-// }
+    std::ostringstream result("");
 
-// std::string checkForDeadGroups(Board& board) {
+    std::vector<unsigned int> group = getGroup(board, latestPos, latestColour);
+    std::vector<unsigned int> groupNeighbours = getGroupNeighbours(board, group, latestColour);
+    std::vector<unsigned int> oppositeColourNeighbours = getOppositeColourNeighbours(board, groupNeighbours);
+    std::vector<unsigned int> liberties = getLiberties(board, group);
+    if(oppositeColourNeighbours.empty()) return result.str();
+    
+    char oppositeColour = board.get(oppositeColourNeighbours[0]);
 
-// }
+    bool groupDeleted = false;
+
+    for(auto& n : oppositeColourNeighbours) {
+
+        std::vector<unsigned int> oppositeGroup = getGroup(board, n, oppositeColour);
+        std::vector<unsigned int> oppositeGroupNeighbours = getGroupNeighbours(board, oppositeGroup, oppositeColour);
+        std::vector<unsigned int> oppositeGroupLiberties = getLiberties(board, oppositeGroupNeighbours);
+
+        // for(auto& l : oppositeGroupLiberties) {
+        //     std::cout << l << " ";
+        // }
+        // std::cout << std::endl;
+
+        if(oppositeGroupLiberties.empty()) {
+
+            // Debug
+            for(auto& l : oppositeGroupNeighbours) {
+                std::cout << l << " ";
+            }
+            std::cout << std::endl;
+            // 
+
+            result << "dead";
+            std::sort(oppositeGroup.begin(), oppositeGroup.end());
+            for(auto& stone : oppositeGroup) {
+                result << " " << int(stone);
+                board.set(stone, 0);
+            }
+            groupDeleted = true;
+        }
+    }
+
+    if (!groupDeleted) {
+        result << "invalid";
+        result << " " << latestPos;
+        board.set(latestPos, 0);
+    }
+
+    return result.str();
+}
+
+// get liberties func ? 
+/*
+    Basically,
+    Save the colour of the last move
+    Check if that last move has opposite colour liberty stones
+    Check if any of those opposite colour stones now have no liberties
+        If not, kill groups
+        Otherwise, check if the placed stone has no liberties
+            If not, kill placed stone
+        Else do nothing 
+*/
 
 #endif 
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -7,7 +7,6 @@
 #include <string>
 #include <algorithm>
 
-
 /*
     9x9 Board Reference
 
@@ -22,55 +21,55 @@
     72  73  74  75  76  77  78  79  80
 */
 
-TEST(getNeighbourPosTest, OOB) {
+TEST(getNeighbourTest, OOB) {
     Board* board = new Board(9); 
     std::vector<unsigned int> a = {};
-    std::vector<unsigned int> b = getNeighbourPos(*board, -1);
-    std::vector<unsigned int> c = getNeighbourPos(*board, 81);
+    std::vector<unsigned int> b = getNeighbours(*board, -1);
+    std::vector<unsigned int> c = getNeighbours(*board, 81);
     ASSERT_EQ(a, b);
     ASSERT_EQ(a, c);
 
     board = new Board(13); 
     std::vector<unsigned int> d = {};
-    std::vector<unsigned int> e = getNeighbourPos(*board, -1);
-    std::vector<unsigned int> f = getNeighbourPos(*board, 169);
+    std::vector<unsigned int> e = getNeighbours(*board, -1);
+    std::vector<unsigned int> f = getNeighbours(*board, 169);
     ASSERT_EQ(d, e);
     ASSERT_EQ(d, f);
 
     board = new Board(19); 
     std::vector<unsigned int> g = {};
-    std::vector<unsigned int> h = getNeighbourPos(*board, -1);
-    std::vector<unsigned int> i = getNeighbourPos(*board, 361);
+    std::vector<unsigned int> h = getNeighbours(*board, -1);
+    std::vector<unsigned int> i = getNeighbours(*board, 361);
     ASSERT_EQ(g, h);
     ASSERT_EQ(g, i);
 }
 
-TEST(getNeighbourPosTest, BoundarySingleStone) {
+TEST(getNeighbourTest, BoundarySingleStone) {
     Board* board = new Board(9); 
-    std::vector<unsigned int> a = getNeighbourPos(*board, 0);
+    std::vector<unsigned int> a = getNeighbours(*board, 0);
     std::vector<unsigned int> b = {1, 9};
     std::sort(a.begin(), a.end());
     std::sort(b.begin(), b.end());
     ASSERT_EQ(a, b);
 
     board = new Board(9); 
-    std::vector<unsigned int> c = getNeighbourPos(*board, 9);
+    std::vector<unsigned int> c = getNeighbours(*board, 9);
     std::vector<unsigned int> d = {0, 10, 18};
     std::sort(c.begin(), c.end());
     std::sort(d.begin(), d.end());
     ASSERT_EQ(c, d);
 
     board = new Board(9); 
-    std::vector<unsigned int> e = getNeighbourPos(*board, 80);
+    std::vector<unsigned int> e = getNeighbours(*board, 80);
     std::vector<unsigned int> f = {71, 79};
     std::sort(e.begin(), e.end());
     std::sort(f.begin(), f.end());
     ASSERT_EQ(e, f);
 }
 
-TEST(getNeighbourPosTest, NonBoundarySingleStone) {
+TEST(getNeighboursTest, NonBoundarySingleStone) {
     Board* board = new Board(9); 
-    std::vector<unsigned int> a = getNeighbourPos(*board, 11);
+    std::vector<unsigned int> a = getNeighbours(*board, 11);
     std::vector<unsigned int> b = {2, 10, 12, 20};
     std::sort(a.begin(), a.end());
     std::sort(b.begin(), b.end());
@@ -176,7 +175,7 @@ TEST(getGroupTest, Group) {
     ASSERT_EQ(got, want);
 }
 
-TEST(getLibertiesTest, Group) {
+TEST(getGroupNeighboursTest, Group) {
     Board* board;
     std::vector<unsigned int> got;
     std::vector<unsigned int> want;
@@ -185,7 +184,7 @@ TEST(getLibertiesTest, Group) {
     board->set(0, 1);
     board->set(1, 1);
     board->set(9, 1);
-    got = getLiberties(*board, {0, 1, 9}, 1);
+    got = getGroupNeighbours(*board, {0, 1, 9}, 1);
     want = {2, 10, 18};
     ASSERT_EQ(got, want);  
     
@@ -201,94 +200,169 @@ TEST(getLibertiesTest, Group) {
     board->set(48, 1);
     board->set(49, 1);
     board->set(50, 1);
-    got = getLiberties(*board, {30, 31, 32, 39, 41, 42, 43, 48, 49, 50}, 1);
+    got = getGroupNeighbours(*board, {30, 31, 32, 39, 41, 42, 43, 48, 49, 50}, 1);
     std::sort(got.begin(), got.end());
     want = {21, 22, 23, 29, 33, 34, 38, 40, 44, 47, 51, 52, 57, 58, 59};
     ASSERT_EQ(got, want);   
 }
 
-/*
-    9x9 Board Reference
+TEST(getLibertiesTest, Group) {
+    Board* board;
+    std::vector<unsigned int> got;
+    std::vector<unsigned int> want;
 
-    0   1   2   3   4   5   6   7   8
-    9   10  11  12  13  14  15  16  17
-    18  19  20  21  22  23  24  25  26
-    27  28  29  30  31  32  33  34  35
-    36  37  38  39  40  41  42  43  44
-    45  46  47  48  49  50  51  52  53
-    54  55  56  57  58  59  60  61  62
-    63  64  65  66  67  68  69  70  71
-    72  73  74  75  76  77  78  79  80
-*/
+    board = new Board(9); 
+    board->set(0, 1);
+    got = getLiberties(*board, {1, 9});
+    want = {1, 9};
+    std::sort(got.begin(), got.end());
+    std::sort(want.begin(), want.end());
+    ASSERT_EQ(got, want);
 
+    board = new Board(9); 
+    board->set(0, 1);
+    board->set(1, 2);
+    got = getLiberties(*board, {1, 9});
+    want = {9};
+    std::sort(got.begin(), got.end());
+    ASSERT_EQ(got, want);
 
-// TEST(isAliveTest, AliveSingleStone) {
-//     Board* board = new Board(9);
-//     board->set(0, 1);
-//     ASSERT_TRUE(isAlive(*board, 0));
+    board = new Board(9); 
+    board->set(30, 1);
+    board->set(31, 1);
+    board->set(32, 1);
+    board->set(39, 1);
+    board->set(41, 1);
+    board->set(42, 1);
+    board->set(43, 1);
+    board->set(48, 1);
+    board->set(49, 1);
+    board->set(50, 1);
+    board->set(22, 2);
+    board->set(44, 2);
+    board->set(57, 2);
+    got = getLiberties(*board, {21, 22, 23, 29, 33, 34, 38, 40, 44, 47, 51, 52, 57, 58, 59});
+    std::sort(got.begin(), got.end());
+    want = {21, 23, 29, 33, 34, 38, 40, 47, 51, 52, 58, 59};
+    ASSERT_EQ(got, want); 
+}
 
-//     board = new Board(9);
-//     board->set(0, 1);
-//     board->set(1, 2);
-//     ASSERT_TRUE(isAlive(*board, 0));
+TEST(getOppositeColourNeighboursTest, Group) {
+    Board* board;
+    std::vector<unsigned int> got;
+    std::vector<unsigned int> want;
 
-//     board = new Board(9);
-//     board->set(3, 1);
-//     ASSERT_TRUE(isAlive(*board, 3));
-// }
+    board = new Board(9); 
+    board->set(0, 1);
+    got = getOppositeColourNeighbours(*board, {1, 9});
+    want = {};
+    std::sort(got.begin(), got.end());
+    ASSERT_EQ(got, want);
 
-// TEST(isAliveTest, DeadSingleStone) {
-//     Board* board = new Board(9);
-//     board->set(0, 1);
-//     board->set(1, 2);
-//     board->set(9, 2);
-//     ASSERT_FALSE(isAlive(*board, 0));
+    board = new Board(9); 
+    board->set(0, 1);
+    board->set(1, 2);
+    got = getOppositeColourNeighbours(*board, {1, 9});
+    want = {1};
+    std::sort(got.begin(), got.end());
+    ASSERT_EQ(got, want);
 
-//     board = new Board(9);
-//     board->set(11, 1);
-//     board->set(2, 2);
-//     board->set(10, 2);
-//     board->set(12, 2);
-//     board->set(20, 2);
-//     ASSERT_FALSE(isAlive(*board, 11));
-// }
+    board = new Board(9); 
+    board->set(30, 1);
+    board->set(31, 1);
+    board->set(32, 1);
+    board->set(39, 1);
+    board->set(41, 1);
+    board->set(42, 1);
+    board->set(43, 1);
+    board->set(48, 1);
+    board->set(49, 1);
+    board->set(50, 1);
+    board->set(22, 2);
+    board->set(44, 2);
+    board->set(57, 2);
+    got = getOppositeColourNeighbours(*board, {21, 22, 23, 29, 33, 34, 38, 40, 44, 47, 51, 52, 57, 58, 59});
+    std::sort(got.begin(), got.end());
+    want = {22, 44, 57};
+    ASSERT_EQ(got, want);    
+}
 
-// TEST(isAliveTest, AliveGroup) {
-//     Board* board = new Board(9);
-//     board->set(0, 1);
-//     board->set(1, 1);
-//     board->set(9, 1); 
-//     ASSERT_TRUE(isAlive(*board, 0));
+TEST(removeDeadStonesTest, Group) {
+    Board* board;
+    std::string got;
+    std::string want;
 
-//     board = new Board(9);
-//     board->set(13, 2);
-//     board->set(14, 2);
-//     board->set(4, 1);
-//     board->set(5, 1);
-//     board->set(12, 1);
-//     board->set(15, 1);
-//     board->set(22, 1);
-//     ASSERT_TRUE(isAlive(*board, 13));
-// }
+    board = new Board(9);
+    board->set(0, 1);
+    board->set(1, 1);
+    board->set(9, 1);
+    board->set(10, 1);
 
-// TEST(isAliveTest, DeadGroup) {
-//     Board* board = new Board(9);
-//     board->set(0, 1);
-//     board->set(1, 1);
-//     board->set(9, 1); 
-//     board->set(2, 2);
-//     board->set(10, 2);
-//     board->set(18, 2);
-//     ASSERT_FALSE(isAlive(*board, 0));
+    board->set(2, 2);
+    board->set(11, 2);
+    board->set(18, 2);
+    board->set(19, 2); 
 
-//     board = new Board(9);
-//     board->set(13, 2);
-//     board->set(14, 2);
-//     board->set(4, 1);
-//     board->set(5, 1);
-//     board->set(12, 1);
-//     board->set(15, 1);
-//     board->set(22, 1);
-//     board->set(23, 1);
-//     ASSERT_FALSE(isAlive(*board, 13));
-// }
+    got = removeDeadStones(*board, 19, 2);
+    want = "dead 0 1 9 10";
+
+    ASSERT_EQ(got, want);
+
+    board = new Board(9);
+    board->set(30, 1);
+    board->set(31, 1);
+    board->set(32, 1);
+    board->set(39, 1);
+    board->set(41, 1);
+    board->set(48, 1);
+    board->set(49, 1);
+    board->set(50, 1);
+
+    board->set(21, 2);
+    board->set(22, 2);
+    board->set(23, 2);
+    board->set(29, 2); 
+    board->set(33, 2);
+    board->set(38, 2);
+    board->set(40, 2);
+    board->set(42, 2);
+    board->set(47, 2); 
+    board->set(51, 2);
+    board->set(57, 2);
+    board->set(58, 2);
+    board->set(59, 2); 
+
+    got = removeDeadStones(*board, 40, 2);
+    want = "dead 30 31 32 39 41 48 49 50";
+
+    ASSERT_EQ(got, want);
+
+    board = new Board(9);
+    board->set(30, 1);
+    board->set(31, 1);
+    board->set(32, 1);
+    board->set(39, 1);
+    board->set(41, 1);
+    board->set(48, 1);
+    board->set(49, 1);
+    board->set(50, 1);
+
+    board->set(21, 2);
+    board->set(22, 2);
+    board->set(23, 2);
+    board->set(29, 2); 
+    
+    board->set(38, 2);
+    board->set(40, 2);
+    board->set(42, 2);
+    board->set(47, 2); 
+    board->set(51, 2);
+    board->set(57, 2);
+    board->set(58, 2);
+    board->set(59, 2); 
+
+    got = removeDeadStones(*board, 40, 2);
+    want = "invalid 40";
+
+    ASSERT_EQ(got, want);
+}
